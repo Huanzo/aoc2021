44b07c2f8f1dba630d4e9b474bebbd2cf706c9fc
Huanzo <huanzodev@gmail.com>
2021-12-01 06:40:41 +0100

    day 1

--- src/day_1.jl
+++ src/day_1.jl
@@ -1,12 +1,29 @@
 include(joinpath(readchomp(`git rev-parse --show-toplevel`), "aoc.jl"))
 import .Aoc: @aoc
 
-function p1(input)
-	nothing
+function sliding(in, w)
+	((@view in[i:i+w-1]) for i in 1:1:length(in)-w+1)
 end
 
-function p2(input)
+function day1(in, w)
+	meassurements = parse.(Int, in)
+	prev = sum(meassurements[begin:begin+w-1])
+	c = 0
+	for i ∈ sliding(meassurements, w)
+		s = sum(i)
+		s > prev && (c += 1)
+		prev = s
+	end
+	c
+end
 
+function p1(input)
+	day1(input, 1)
+end
+
+
+function p2(input)
+	day1(input, 3)
 end
 
 @aoc(2021, 1)
